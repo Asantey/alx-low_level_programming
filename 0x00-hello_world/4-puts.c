1b2215b1a098aa071394bf066b89e60010a74391
root <you@example.com>
2022-03-10 17:06:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-*main -prints out a string to stdout.
 *
-*/
-int main (void)
+*will printa string to stdout.
+*
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
