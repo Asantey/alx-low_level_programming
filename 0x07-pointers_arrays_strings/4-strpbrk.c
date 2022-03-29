0aa5d87093e03d070654af2b487888b3363882de
root <you@example.com>
2022-03-28 17:29:34 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
-#include <main.h>
+#include "holberton.h"
+#include "main.h"
 
 /**
  *  _strpbrk - bytes
@@ -6,20 +7,19 @@
  * @accept: pointer to char
  * Return: NULL
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-
-	
-		for (i = 0; s[i]; i++)
+	unsigned int i, j;
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
 		{
-			for (j=0; accept [j]; j++)
-			{
 			if (s[i] == accept[j])
 				break;
-			}
-			if (accept[j])
-                             return (s+i);
 		}
-		return (0);
+		if (accept[j])
+			return (s + i);
+	}
+	return (0);
 }
